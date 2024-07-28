64b7f8e697d2656d125f1b5bb95f592402edfb62
Twinkling-Lexie <1315598714@qq.com>
2024-07-28 14:32:50 +0800

    add new files

---
+++ hello_world.cu
@@ -0,0 +1,19 @@
+/*
+    hello_world.cu
+*/
+
+#include <stdio.h>
+
+// Each of these ten threads will print the text once.
+__global__ void hello_world(void){
+    printf("printed by %d : GPU: Hello world!\n",threadIdx.x);
+    // printf("GPU: Hello world!\n");
+}
+
+int main(){
+    // Here "CPU: Hello world!\n" is a string printed by CPU(Host).
+    printf("CPU: Hello world!\n");
+    hello_world<<<1,10>>>();
+    cudaDeviceReset();
+    return 0;
+}
